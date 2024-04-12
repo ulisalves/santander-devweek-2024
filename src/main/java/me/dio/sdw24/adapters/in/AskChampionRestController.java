a39071cd0717edb70cc0ae6d9fb54fcaeadaa711
Ulisses Alves <ulisalves@gmail.com>
2024-04-12 01:09:33 -0300

    implantation enviroment variable IA generative

---
+++ src/main/java/me/dio/sdw24/adapters/in/AskChampionRestController.java
@@ -0,0 +1,28 @@
+package me.dio.sdw24.adapters.in;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import me.dio.sdw24.application.AskChampionUseCase;
+import me.dio.sdw24.application.ListChampionsUseCase;
+import me.dio.sdw24.domain.model.Champion;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Campeões", description = "Endpoints do domínio de Campeões do LOL.")
+@RestController
+@RequestMapping("/champions")
+public record AskChampionRestController(AskChampionUseCase useCase) {
+
+    @CrossOrigin
+    @PostMapping("/{championId}/ask")
+    public AskChampionResponse askChampion(
+            @PathVariable Long championId,
+            @RequestBody AskChampionRequest request) {
+        String answer = useCase.askChampion(championId, request.question());
+
+        return new AskChampionResponse(answer);
+    }
+
+    public record AskChampionRequest (String question){ }
+    public record AskChampionResponse (String answer) { }
+}
